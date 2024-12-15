72ff1decf80d6206da33be70994c815b24a8a69c
ndia1019 <naflanagan@cps.edu>
2024-12-15 01:25:35 +0000

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
 let  left = document.querySelector('.left');
 let  right = document.querySelector('.right');
 let  leftRight = document.querySelector('.leftRight');
-
+let text= document.querySelector('p');
 let  pizza = document.querySelector('.pizza');
 pizza.style.display ="none";
+let burp = document.querySelector('.burp');
+burp.style.display="none";
 
 left.addEventListener('click', function() {
+
+    console.log("left is clicked")
 
     leftRight.style.display ="none";
     left.style.display ="none";
     right.style.display ="none";
-   
     pizza.style.display ="block";
+  
+text.innerHTML= "double click to eat or find something heathy";
+});
 
+pizza.addEventListener('dblclick', function(){
+console.log("pizza dlb clicked ")
+pizza.style.display ="none"; 
+left.style.display ="none";
+right.style.display="none";
+burp.style.display="block";
+text.innerHTML= "thats good pizza"
 });
